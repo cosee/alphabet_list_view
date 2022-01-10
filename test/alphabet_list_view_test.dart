2d55aaf6f24637ca88fd6467aef9ad2d25d951cd
Tobias Wawrzinek <tobias.wawrzinek@cosee.biz>
2022-01-10 11:25:54 +0100

    Edited ci config

--- test/alphabet_list_view_test.dart
+++ test/alphabet_list_view_test.dart
@@ -1,48 +1,37 @@
+import 'package:alphabet_list_view/alphabet_list_view.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter/material.dart';
-
 void main() {
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows building and interacting
-  // with widgets in the test environment.
-  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
-    // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(const MyWidget(title: 'T', message: 'M'));
 
-    // Create the Finders.
-    final titleFinder = find.text('T');
-    final messageFinder = find.text('M');
 
-    // Use the `findsOneWidget` matcher provided by flutter_test to
-    // verify that the Text widgets appear exactly once in the widget tree.
-    expect(titleFinder, findsOneWidget);
-    expect(messageFinder, findsOneWidget);
+
+  testWidgets('OnlyUniqueSymbols', (WidgetTester tester) async {
+    await tester.pumpWidget(const Home());
+    final symbolAFinder = find.text('A');
+    expect(symbolAFinder, findsNWidgets(2));
   });
+
+
+
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({
-    Key? key,
-    required this.title,
-    required this.message,
-  }) : super(key: key);
+List<AlphabetListViewItemGroup> alphabetListViewItemGroups = [
+  AlphabetListViewItemGroup(
+    tag: 'A',
+    children: [
+      const Text('B'),
+    ],
+  ),
+];
 
-  final String title;
-  final String message;
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Center(
-          child: Text(message),
-        ),
-      ),
+      home: Scaffold(body: AlphabetListView(items: alphabetListViewItemGroups)),
     );
   }
 }
