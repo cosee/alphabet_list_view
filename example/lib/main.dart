20e37702eeffd142e6ff81b14197e619255d5dfa
Quoc Huynh <quoc.huynh@cosee.biz>
2023-09-06 11:57:10 +0200

    Add example for other platforms

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AlphabetListView demo',
+      title: 'AlphabetListView Example',
       theme: CustomTheme.theme(context),
       home: const Home(),
     );
